fd04a5eee4cab3e64bd414cebdc44abba3a3e2da
Valeriia <valeriia.vp1@yandex.ru>
2025-10-29 12:03:01 +0100

    Updated the windows view

---
+++ Warehouse Management System/EmployeeView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Warehouse_Management_System;
+
+public partial class EmployeeView : Window
+{
+    public EmployeeView()
+    {
+        InitializeComponent();
+    }
+}
--- Warehouse Management System/MainWindow.xaml.cs
+++ Warehouse Management System/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Warehouse_Management_System;
 
@@ -19,5 +10,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void EmployeeButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        EmployeeView employeeView = new EmployeeView();
+        employeeView.Show();   
+        this.Close();
     }
 }
